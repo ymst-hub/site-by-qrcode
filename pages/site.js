33d868a291f656994b3f757f2e1af4ef63a82b4c
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-10-07 23:19:58 +0900

    JSを読み込まないように設定

--- pages/site.js
+++ pages/site.js
@@ -1,18 +1,37 @@
 import { useRouter } from "next/router";
 
+
 /**
  * QRで読み取ったQRコードを読み取って表示する。
  * @returns HTMLに変換したサイトを表示
  */
 
-//todo 読み込みの安全対策
 
 export default function Site(){
+    //禁止するコード
+    const ban_codes = ["<script", "<input"]
+
     const router = useRouter();
-    console.log(router.query.data)
+    let qr_html_code = find_ban_code(ban_codes,router.query.data)
+
     return (
         <div>
-            <div dangerouslySetInnerHTML={{ __html: router.query.data }} />
+            <div dangerouslySetInnerHTML={{ __html: qr_html_code }} />
         </div>
     )
+}
+
+/**
+ * 
+ * @param {*} ban_codes 禁止コードを配列にして渡します。
+ * @param {*} str qrで読み取ったhtmlコードを格納します
+ * @returns 禁止コードを含んでいた場合、文字列を、含んでいない場合、そのままHTMLを返します。
+ */
+function find_ban_code(ban_codes,html_code){
+    ban_codes.array.forEach(code => {
+        if(html_code.indexOf(code) != -1){
+            return "<h1>Sorry can't display. This code have javascript</h1>"
+        }
+    });
+    return html_code
 }
