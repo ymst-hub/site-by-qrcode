8d79d767e0115a7bf7214b30ff33eba87360490c
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-10-10 23:21:42 +0900

    コミット

--- pages/site.js
+++ pages/site.js
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 export default function Site() {
     //禁止するコード
     const ban_codes = ['<script', '<input','href']
+
     const router = useRouter();
     let qr_html_code = find_ban_code(router.query.data, ...ban_codes)
 
@@ -22,20 +23,19 @@ export default function Site() {
 
 /**
  * 
- * @param {*} ban_codes 禁止コードを配列にして渡します。
+ * @param {*} ban_codes 禁止文字列を配列にして渡します。
  * @param {*} str qrで読み取ったhtmlコードを格納します
- * @returns 禁止コードを含んでいた場合、文字列を、含んでいない場合、そのままHTMLを返します。
+ * @returns 禁止コードを含んでいた場合、error_htmlを、含んでいない場合、そのままHTMLを返します。
  */
 function find_ban_code(html_code, ...ban_codes) {
+    let error_html = '<h1>Sorry can not display.</h1>'
     if (html_code === undefined || ban_codes === undefined){
-        return '<h1>Sorry can not display.</h1>'
+        return error_html
     }
 
     for (let i = 0; i < ban_codes.length; i++) {
-        let buf = html_code.indexOf(ban_codes[i])
-        console.log(buf)
-        if (buf != -1) {
-            return '<h1>Sorry can not display. </h1>'
+        if (html_code.indexOf(ban_codes[i]) != -1) {
+            return error_html
         }
     }
     return html_code
