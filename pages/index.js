12d904e6e2855aad206c4c7e7262c0d6e097dd44
ymst-hub <64212002+ymst-hub@users.noreply.github.com>
2022-10-10 23:11:02 +0900

    コミット

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,16 @@
 import { useRouter } from 'next/router';
 import { QrReader } from 'react-qr-reader';
-import '../styles/globals.css' // site.jsでは個別に設定するため
 
 export default function Home() {
-  const router = useRouter();
+  const router = useRouter()
+  let router_use_flg = true
   return (
     <div>
       <h1>QRCodeからサイトを描画します</h1>
-      <QrReader delay={10000}
+      <QrReader Delay={5000}
         onResult={(result) => {
-          if (!!result) {
-            console.log(result?.text)
+          if (!!result && router_use_flg) {
+            router_use_flg = false
             router.push({ pathname: "site", query: {data : result?.text } }, "site")
           }
         }}
--- pages/site.js
+++ pages/site.js
@@ -7,12 +7,11 @@ import { useRouter } from "next/router";
  */
 
 
-export default function Site(){
+export default function Site() {
     //禁止するコード
-    const ban_codes = ["<script", "<input"]
-
+    const ban_codes = ['<script', '<input','href']
     const router = useRouter();
-    let qr_html_code = find_ban_code(ban_codes,router.query.data)
+    let qr_html_code = find_ban_code(router.query.data, ...ban_codes)
 
     return (
         <div>
@@ -27,11 +26,17 @@ export default function Site(){
  * @param {*} str qrで読み取ったhtmlコードを格納します
  * @returns 禁止コードを含んでいた場合、文字列を、含んでいない場合、そのままHTMLを返します。
  */
-function find_ban_code(ban_codes,html_code){
-    ban_codes.array.forEach(code => {
-        if(html_code.indexOf(code) != -1){
-            return "<h1>Sorry can't display. This code have javascript</h1>"
+function find_ban_code(html_code, ...ban_codes) {
+    if (html_code === undefined || ban_codes === undefined){
+        return '<h1>Sorry can not display.</h1>'
+    }
+
+    for (let i = 0; i < ban_codes.length; i++) {
+        let buf = html_code.indexOf(ban_codes[i])
+        console.log(buf)
+        if (buf != -1) {
+            return '<h1>Sorry can not display. </h1>'
         }
-    });
+    }
     return html_code
 }
